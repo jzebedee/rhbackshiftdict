a67839c52ceb4a4b5102f4ce54a1b37d877195cc
scorpion <scorpion.jze@gmail.com>
2014-08-16 21:17:29 -0500

    Single struct array instead of individual arrays

--- rhbackshiftdict/RobinHoodDictionary.cs
+++ rhbackshiftdict/RobinHoodDictionary.cs
@@ -12,36 +12,49 @@ namespace rhbackshiftdict
         const float LOAD_FACTOR = 0.86f;
         const uint SAFE_HASH = 0x80000000;
 
+        struct Entry
+        {
+            public Entry(uint hash, TKey key, TValue value)
+            {
+                this.hash = hash;
+                this.key = key;
+                this.value = value;
+            }
+
+            public readonly uint hash;
+            public readonly TKey key;
+            public readonly TValue value;
+        }
+
         private IEnumerable<KeyValuePair<TKey, TValue>> Entries
         {
             get
             {
                 for (uint i = 0; i < count; i++)
-                    if (hashes[i] != 0)
-                        yield return new KeyValuePair<TKey, TValue>(keys[i], values[i]);
+                    if (buckets[i].hash != 0)
+                        yield return new KeyValuePair<TKey, TValue>(buckets[i].key, buckets[i].value);
             }
         }
 
-        private uint[] hashes;
-        private TKey[] keys;
-        private TValue[] values;
-
+        private Entry[] buckets;
         private uint count;
         private uint countUsed;
 
         private uint growAt;
         private uint shrinkAt;
 
+        private readonly IEqualityComparer<TKey> KeyComparer;
+
         public RobinHoodDictionary()
         {
             Clear();
         }
-        public RobinHoodDictionary(uint size)
-            : this()
+        public RobinHoodDictionary(int size, IEqualityComparer<TKey> comparer = null) : this((uint)size, comparer) { }
+        public RobinHoodDictionary(uint size, IEqualityComparer<TKey> comparer = null)
         {
+            KeyComparer = comparer ?? EqualityComparer<TKey>.Default;
             Resize(NextPow2(size));
         }
-        public RobinHoodDictionary(int size) : this((uint)size) { }
 
         private void Resize(uint newSize)
         {
@@ -53,14 +66,10 @@ namespace rhbackshiftdict
             }
 #endif
             var oldCount = count;
-            var oldHashes = hashes;
-            var oldKeys = keys;
-            var oldValues = values;
+            var oldBuckets = buckets;
 
             count = newSize;
-            hashes = new uint[newSize];
-            keys = new TKey[newSize];
-            values = new TValue[newSize];
+            buckets = new Entry[newSize];
 
             growAt = Convert.ToUInt32(newSize * LOAD_FACTOR);
             shrinkAt = newSize >> 2;
@@ -68,13 +77,13 @@ namespace rhbackshiftdict
             if (countUsed > 0 && newSize != 0)
             {
                 Debug.Assert(countUsed <= newSize);
-                Debug.Assert(oldHashes != null && oldKeys != null && oldValues != null);
+                Debug.Assert(oldBuckets != null);
 
                 countUsed = 0;
 
                 for (uint i = 0; i < oldCount; i++)
-                    if (oldHashes[i] != 0)
-                        PutInternal(oldHashes[i], oldKeys[i], oldValues[i], false, false);
+                    if (oldBuckets[i].hash != 0)
+                        PutInternal(oldBuckets[i], false, false);
             }
         }
 
@@ -97,15 +106,15 @@ namespace rhbackshiftdict
                 {
                     indexCurrent = (indexInit + i) & (count - 1);
 
-                    if (hashes[indexCurrent] != 0)
+                    if (buckets[indexCurrent].hash != 0)
                         probeDistance = DistanceToInitIndex(indexCurrent);
 
                     if (i > probeDistance)
                         break;
 
-                    if (hash == hashes[indexCurrent] && key.Equals(keys[indexCurrent]))
+                    if (hash == buckets[indexCurrent].hash && KeyComparer.Equals(key, buckets[indexCurrent].key))
                     {
-                        value = values[indexCurrent];
+                        value = buckets[indexCurrent].value;
                         return true;
                     }
                 }
@@ -123,13 +132,13 @@ namespace rhbackshiftdict
             if (countUsed == growAt)
                 ResizeNext();
 
-            return PutInternal(GetHash(key), key, val, canReplace, true);
+            return PutInternal(new Entry(GetHash(key), key, val), canReplace, true);
         }
 
-        bool PutInternal(uint hash, TKey key, TValue value, bool canReplace, bool checkDuplicates)
+        bool PutInternal(Entry entry, bool canReplace, bool checkDuplicates)
         {
             uint
-                indexInit = hash & (count - 1),
+                indexInit = entry.hash & (count - 1),
                 indexCurrent,
 
                 probeDistance = 0,
@@ -138,17 +147,17 @@ namespace rhbackshiftdict
             for (uint i = 0; i < count; i++)
             {
                 indexCurrent = (indexInit + i) & (count - 1);
-                if (hashes[indexCurrent] == 0)
+                if (buckets[indexCurrent].hash == 0)
                 {
                     countUsed++;
-                    Insert(indexCurrent, hash, key, value);
+                    buckets[indexCurrent] = entry;
                     return true;
                 }
-                else if (checkDuplicates && hash == hashes[indexCurrent] && key.Equals(keys[indexCurrent]))
+                else if (checkDuplicates && entry.hash == buckets[indexCurrent].hash && KeyComparer.Equals(entry.key, buckets[indexCurrent].key))
                 {
                     if (canReplace)
                     {
-                        values[indexCurrent] = value;
+                        buckets[indexCurrent] = entry;
                         return true;
                     }
                     else
@@ -163,11 +172,7 @@ namespace rhbackshiftdict
                     if (probeCurrent > probeDistance)
                     {
                         probeCurrent = probeDistance + 1;
-
-                        Swap(ref hashes[indexCurrent], ref hash);
-
-                        Swap(ref keys[indexCurrent], ref key);
-                        Swap(ref values[indexCurrent], ref value);
+                        Swap(ref buckets[indexCurrent], ref entry);
                     }
                     else probeCurrent++;
                 }
@@ -192,7 +197,7 @@ namespace rhbackshiftdict
                 for (uint i = 0; i < count; i++)
                 {
                     indexCurrent = (indexInit + i) & (count - 1);
-                    if (hash == hashes[indexCurrent] && key.Equals(keys[indexCurrent]))
+                    if (hash == buckets[indexCurrent].hash && KeyComparer.Equals(key, buckets[indexCurrent].key))
                     {
                         found = true;
                         break;
@@ -207,16 +212,13 @@ namespace rhbackshiftdict
                         index_previous = (indexCurrent + i - 1) & (count - 1);
                         index_swap = (indexCurrent + i) & (count - 1);
 
-                        if (hashes[index_swap] == 0 || DistanceToInitIndex(index_swap) == 0)
+                        if (buckets[index_swap].hash == 0 || DistanceToInitIndex(index_swap) == 0)
                         {
-                            Delete(index_previous);
+                            buckets[index_previous] = default(Entry);
                             break;
                         }
 
-                        Swap(ref hashes[index_previous], ref hashes[index_swap]);
-
-                        Swap(ref keys[index_previous], ref keys[index_swap]);
-                        Swap(ref values[index_previous], ref values[index_swap]);
+                        Swap(ref buckets[index_previous], ref buckets[index_swap]);
                     }
 
                     if (--countUsed == shrinkAt)
@@ -229,8 +231,9 @@ namespace rhbackshiftdict
 
         private uint DistanceToInitIndex(uint indexStored)
         {
-            Debug.Assert(hashes[indexStored] != 0);
-            uint indexInit = hashes[indexStored] & (count - 1);
+            Debug.Assert(buckets[indexStored].hash != 0);
+
+            uint indexInit = buckets[indexStored].hash & (count - 1);
             if (indexInit <= indexStored)
                 return indexStored - indexInit;
             else
@@ -240,20 +243,6 @@ namespace rhbackshiftdict
         private void ResizeNext()
         {
             Resize(count == 0 ? 1 : count * 2);
-        }
-
-        private void Delete(uint index)
-        {
-            hashes[index] = 0;
-            keys[index] = default(TKey);
-            values[index] = default(TValue);
-        }
-
-        private void Insert(uint index, uint hash, TKey key, TValue value)
-        {
-            hashes[index] = hash;
-            keys[index] = key;
-            values[index] = value;
         }
 
         private uint GetHash(TKey o)
